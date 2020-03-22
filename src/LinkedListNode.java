3c562bede58fd26496755961f5a1d5924b2b88ef
lourdhurudin <donrudin@gmail.com>
2020-03-21 20:02:09 -0500

    Added Reverse List method

--- src/LinkedListNode.java
+++ src/LinkedListNode.java
@@ -57,6 +57,30 @@ public class LinkedListNode {
         return false;
     }
 
+    public static LinkedListNode reverse(LinkedListNode headOfList) {
+        LinkedListNode currentNode = headOfList;
+        LinkedListNode previousNode = null;
+        LinkedListNode nextNode = null;
+
+        // until we have 'fallen off' the end of the list
+        while (currentNode != null) {
+
+            // copy a pointer to the next element
+            // before we overwrite currentNode.next
+            nextNode = currentNode.next;
+
+            // reverse the 'next' pointer
+            currentNode.next = previousNode;
+
+            // step forward in the list
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        return previousNode;
+    } 
+
+
 }
 
 
